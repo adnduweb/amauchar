7f64760e2461822fac2aa07417fbecdb01bb65c5
Fabrice Loru <34213953+adnduweb@users.noreply.github.com>
2022-05-15 14:19:13 +0200

    Test production

--- resources/Views/backend/metronic/Components/form-action-footer.php
+++ resources/Views/backend/metronic/Components/form-action-footer.php
@@ -1,5 +1,5 @@
-<button type="reset" id="kt_<?= $type ?? '' ?>_cancel" class="btn btn-light me-3 kt_<?= $type ?? '' ?>_cancel"> <?= ucfirst(lang('Core.discard')); ?></button>
-<button type="submit" id="kt_<?= $type ?? '' ?>_submit" class="btn btn-primary kt_<?= $type ?? '' ?>_submit">
+<button type="reset" id="kt_<?= $type ?? '' ?>_cancel" class="btn btn-light btn-sm me-3 kt_<?= $type ?? '' ?>_cancel"> <?= ucfirst(lang('Core.discard')); ?></button>
+<button type="submit" id="kt_<?= $type ?? '' ?>_submit" class="btn btn-primary btn-sm kt_<?= $type ?? '' ?>_submit">
     <span class="indicator-label"> <?= ucfirst(lang('Core.saveForm')); ?></span>
     <span class="indicator-progress"><?= ucfirst(lang('Core.pleaseWait')); ?> 
         <span class="spinner-border spinner-border-sm align-middle ms-2"></span>
